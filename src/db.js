bcd743a323e05b6f1aefcf49d347e54dac51a4a2
Eduardo Yrigoyen <eyrigoyen78@gmail.com>
2022-09-21 18:43:38 -0500

    configuracion de despliegue

--- src/db.js
+++ src/db.js
@@ -1,9 +1,17 @@
 import { createPool } from "mysql2/promise";
+import {
+    BD_HOST,
+    DB_PORT,
+    DB_DATABASE,
+    DB_USER,
+    DB_PASSWORD
+
+} from './config.js'
 
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Root@12345',
-    port: 3306,
-    database: 'companydb'
+    host: BD_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    database: DB_DATABASE
 })
